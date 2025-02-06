0b4622265ece32e427b87add691eebe274bd6fc1
ivanostorari <programadordev41@gmail.com>
2025-02-06 08:56:41 -0400

    Exercicio 02

---
+++ Desafio Nível 01/Exercicio 02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        // Solicitar o nome do usuário
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        // Solicitar o sobrenome do usuário
+        Console.Write("Digite seu sobrenome: ");
+        string sobrenome = Console.ReadLine();
+
+        // Concatenar o nome e o sobrenome
+        string nomeCompleto = nome + " " + sobrenome;
+
+        // Exibir o nome completo
+        Console.WriteLine($"Nome completo: {nomeCompleto}");
+    }
+}
