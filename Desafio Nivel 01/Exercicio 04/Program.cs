be756cafae2c16117599830fcad6a215dc5f0ea8
ivanostorari <programadordev41@gmail.com>
2025-02-06 09:07:52 -0400

    Exercicio 04

---
+++ Desafio Nivel 01/Exercicio 04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        // Solicitar que o usuário digite uma ou mais palavras
+        Console.Write("Digite uma ou mais palavras: ");
+        string entrada = Console.ReadLine();
+
+        // Contar a quantidade de caracteres
+        int quantidadeCaracteres = entrada.Length;
+
+        // Exibir a quantidade de caracteres
+        Console.WriteLine($"Quantidade de caracteres: {quantidadeCaracteres}");
+    }
+}
